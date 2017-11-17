a27a076441814034208883090138a8eeba016913
NetoHaarp <NetoHaarp@LENOVO-PCNETO>
2017-11-16 21:51:17 -0600

    Agregar archivos de proyecto.

---
+++ ClaseFiguras/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseFiguras
+{
+    class Figura
+    {
+       public void Cuadrado()
+        {
+            double lad = 0;
+            Console.WriteLine("Introduce un lado del cuadrado");
+            lad = double.Parse(Console.ReadLine());
+            Console.WriteLine("Area del cuadrado = "+(lad*lad));
+        }
+
+        public void Triangulo()
+        {
+            double bas = 0, altura = 0;
+
+            Console.WriteLine("Introduce la base");
+            bas = double.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce la altura");
+            altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Area del Triangulo = " + (bas*altura)/2);
+        }
+
+        public void Circulo()
+        {
+            double rad = 0;
+
+            Console.WriteLine("Introduce radio");
+            rad = double.Parse(Console.ReadLine());
+            Console.WriteLine("Area del Circulo = " + (13.1416*(rad*rad)));
+        }
+    }
+}
---
+++ ClaseFiguras/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClaseFiguras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double k = 1;
+            Figura f = new Figura();
+            while (k != 0)
+            {
+                Console.WriteLine("¿Que area de figura deceas realizar?");
+                Console.WriteLine("1) Cuadrado \n 2) Triangulo \n 3) Circulo \n 0) Salir");
+                int n = int.Parse(Console.ReadLine());
+                if (n == 1)
+                {
+                    f.Cuadrado();
+                }
+                if (n == 2)
+                {
+                    f.Triangulo();
+                }
+                if (n == 3)
+                {
+                    f.Circulo();
+                }
+                if(n == 0)
+                {
+                    k = n;
+                }
+
+
+
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
